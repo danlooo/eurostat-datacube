e7207c987e6297f08bf6e5ff55248b41a785eae3
Daniel Loos <dloos@bgc-jena.mpg.de>
2026-02-12 10:45:30 +0100

    Fix target codes

--- _targets.R
+++ _targets.R
@@ -71,7 +71,7 @@ list(
       "nama_10r_3gdp", "nama_10r_3gva", "nama_10r_2gvagr",
       "teicp010", "teicp250", "nama_10_nfa_bs",
       "lfst_r_lfu3pers", "demo_r_d3dens", "ilc_li02", "ilc_di11",
-      "nama_10r_2gfcf", "nama_10r_2emhrw", "edat_lfse_04",
+      "nama_10r_2gfcf", "nama_10r_2emhrw", "edat_lfse_04"
     ),
   ),
   tar_target(
